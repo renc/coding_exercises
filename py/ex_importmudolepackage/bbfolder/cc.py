d02f8305cad95a955c1f7a51ee9e944f4bdc38cb
canjiang <canjiang.ren@outlook.com>
2018-10-25 16:14:03 +0800

    update comment

--- py/ex_importmudolepackage/bbfolder/cc.py
+++ py/ex_importmudolepackage/bbfolder/cc.py
@@ -6,7 +6,8 @@ Created on Wed Sep 12 09:25:06 2018
 
 # this . is the current path, which is relative path (always the current folder)
 # 
-# import bb # this will fail
+# import bb # this will fail, because it  depends on the os.getcwd() 
+# which most of time donot equal to this fils's .  
 from . import bb
 
 def printCC():
