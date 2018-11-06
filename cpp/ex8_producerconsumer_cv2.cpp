3d7c330ba5d0b70e707dc5a36d921cf14155108f
canjiang <canjiang.ren@outlook.com>
2018-11-06 17:17:17 +0800

    update my implement of concurrent queue

--- cpp/ex8_producerconsumer_cv2.cpp
+++ cpp/ex8_producerconsumer_cv2.cpp
@@ -6,7 +6,8 @@ ex8_producerconsumer_cv2.cpp is a refactor of that version.
 Producer Consumer with std::queue 
 https://gist.github.com/iikuy/8115191 
 
-g++ filename.cpp -o exename -std=c++11 -pthread 
+g++ filename.cpp -o exename -std=c++11 -pthread -g
+-g for debug
 
 */ 
 #include <thread>
@@ -16,39 +17,87 @@ g++ filename.cpp -o exename -std=c++11 -pthread
 #include <iostream> 
 #include <vector> 
 
+template <typename T>
 class QueueCV {
 public:
-    void push(int n)
+    void push(const T& n)
     {
         {
             std::lock_guard<std::mutex> lk(m_mtx);
             m_queue.push(n);
-        } // Important, unlock before notify, otherwise, the later 
+        } // Important, unlock before notify, otherwise, 
+        // the waiting thread wait up before the mutx is unlock.
         m_cv.notify_all();
+        // vs notify_one() ? multiply customers.
     }
-    int pop() 
+    // T pop() // i use this function signature before, but change to
+    // Using a reference parameter to receive the result is used to transfer ownership
+    //  out of the queue in order to avoid the exception safety issues 
+    // of returning data by-value: if the copy constructor of a by-value return throws, 
+    // then the data has been removed from the queue, but is lost, 
+    // whereas with this approach, the potentially problematic copy is performed 
+    // prior to modifying the queue (see Herb Sutter's Guru Of The Week #8 
+    // for a discussion of the issues). 
+    void pop(T &destination)
     {
         std::unique_lock<std::mutex> lk(m_mtx);
         m_cv.wait(lk, [this](){ return m_queue.empty() == false;});
         // same as
         // while ( ! (q.empty() == false) ) 
         //     m_cv.wait(lk);
-        int iRet = m_queue.front();
-        m_queue.pop();
-        return iRet;
+        //
+        // condition variables can be subject to spurious wake-ups, 
+        // so it is important to check the actual condition being waited for 
+        // when the call to wait returns.
+        // 
+        // if we are here, q is not empty and mtx is locked.
+        
+        //T iRet = m_queue.front();
+        destination = m_queue.front();
+        m_queue.pop();   
+        //return iRet;     
         // this could be blocking (not no-blocking)
     }
+    T& front() {
+        // why return type is reference ? 
+        // https://en.cppreference.com/w/cpp/container/queue/front
+        std::lock_guard<std::mutex> lk(m_mtx);
+        return m_queue.front();
+    } 
+    const T& front() const {
+        std::lock_guard<std::mutex> lk(m_mtx);
+        return m_queue.front();
+    }
+    // renc, why to remove front() and pop() for multiply customers ?
+    // tbb concurrent_queue does not have front() and pop() functions.
+    // https://www.threadingbuildingblocks.org/tutorial-intel-tbb-concurrent-containers
+    // https://www.justsoftwaresolutions.co.uk/threading/implementing-a-thread-safe-queue-using-condition-variables.html
+    // 
+    bool try_pop(T &destination) {
+        std::lock_guard<std::mutex> lk(m_mtx);
+        if (m_queue.empty())
+            return false;
+
+        destination = m_queue.front();
+        m_queue.pop();  
+        return true;
+    }
+    bool empty() const {
+        std::lock_guard<std::mutex> lk(m_mtx);
+        return m_queue.empty();
+    }
+
 private:
     std::mutex m_mtx;
     std::condition_variable m_cv;
 
-    std::queue<int> m_queue; // un-bound
+    std::queue<T> m_queue; // un-bound
 };
 
 class Producer
 {
 public:
-    Producer(QueueCV &q) : m_queue(q) {}
+    Producer(QueueCV<int> &q) : m_queue(q) {}
     void run()
     {
         int iN = 0;
@@ -57,27 +106,28 @@ public:
         }
     }
 private:
-    QueueCV &m_queue;
+    QueueCV<int> &m_queue;
 };
 
 class Consumer
 {
 public:
-    Consumer(QueueCV &q, int iId) : m_queue(q), m_iId(iId) {}
+    Consumer(QueueCV<int> &q, int iId) : m_queue(q), m_iId(iId) {}
     void run()
     {
         while(1) {
-            int iN = m_queue.pop();
+            int iN = -1;
+            m_queue.pop(iN);
             std::cout << "Consumer id: " << m_iId << ", " << iN << std::endl;
         }
     }
 private:
-    QueueCV &m_queue;
+    QueueCV<int> &m_queue;
     int m_iId;
 };
 int main()
 {
-    QueueCV queue;
+    QueueCV<int> queue;
     Producer p(queue);
     Consumer c0(queue, 10);
     Consumer c1(queue, 11);
