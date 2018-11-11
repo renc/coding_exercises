a9ee71399b7995e2872fb16a98d54b66a925a919
renc <rencanjiang@gmail.com>
2018-11-11 22:00:22 +0800

    test2, finsih part A.

--- cpp/test2.cpp
+++ cpp/test2.cpp
@@ -1,4 +1,5 @@
 // Test 2, answer from canjiang.ren@outlook.com 
+// First, thx for sharing this test, really interesting. 
 
 // These assumptions are made:
 // 1. There is a client which communicate with market/exchange server.
@@ -13,16 +14,22 @@
 // 
 
 // Result:
-// Part A. Operation 1. finished. 
-//         Operation 2. donot finish since not understand this operation.
+// Part A. (1). finished. Please check function DerivedListener::hasEnoughXRequestsForLastYSeconds() for details. 
+//         (2). The exact time depends, for example I try to invoke DerivedListener::hasEnoughXRequestsForLastYSeconds()
+//              and get times like: 
+//              Test 1 took time: 0.000249115s. Test 1 took time: 0.000371103s.
 // Part B. All operations are finished.
 
 #include <iostream> 
 #include <assert.h>
 #include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
 #include <queue>
 #include <memory>
 #include <deque>
+#include <unordered_map>
 #include <chrono>
 
 class Listener
@@ -73,7 +80,8 @@ public:
 // one message type for one callback. 
 enum class MsgType {
         tInsertOrderRequest, tReplaceOrderRequest, 
-        tRequestAcknowledged, tRequestRejectd, tOrderFilled 
+        tRequestAcknowledged, tRequestRejectd, tOrderFilled, 
+        tHasXRequestForLastYSeconds
 }; 
 class BasicMsg {
 public:
@@ -81,34 +89,45 @@ public:
 };
 class MsgInsertOrderRequest : public BasicMsg {
 public: 
-    MsgType msgType() const { return MsgType::tInsertOrderRequest;}
+    MsgType msgType() const override { return MsgType::tInsertOrderRequest;}
     int m_id = -1;      // should be private data memeber, and public getXX and setXX(), 
     char m_cSide = 'U'; // unknown
     double m_fPrice; 
     int m_iQuantity;
+    std::chrono::steady_clock::time_point m_requestTime;
 };
 class MsgReplaceOrderRequest : public BasicMsg {
 public: 
-    MsgType msgType() const { return MsgType::tReplaceOrderRequest;}
+    MsgType msgType() const override { return MsgType::tReplaceOrderRequest;}
     int m_iOldId = -1; 
     int m_iNewId = -1;
     int m_iDeltaQuantity = 0; 
+    std::chrono::steady_clock::time_point m_requestTime;
 };
 class MsgRequestAcknowledged: public BasicMsg {
 public:
-    MsgType msgType() const { return MsgType::tRequestAcknowledged; }
+    MsgType msgType() const override { return MsgType::tRequestAcknowledged; }
     int m_iId = -1;
 };
 class MsgRequestRejected: public BasicMsg {
 public:
-    MsgType msgType() const { return MsgType::tRequestRejectd; }
+    MsgType msgType() const override { return MsgType::tRequestRejectd; }
     int m_iId = -1;
 };
 class MsgOrderFilled: public BasicMsg {
 public: 
-    MsgType msgType() const { return MsgType::tOrderFilled; }
+    MsgType msgType() const override { return MsgType::tOrderFilled; }
     int m_iId = -1;
     int m_iQuantityFilled = -1;
+};
+class MsgHasXRequestForLastYSeconds : public BasicMsg 
+{
+public:
+    MsgType msgType() const override { return MsgType::tHasXRequestForLastYSeconds; }
+    int m_iXNumRequest = -1;
+    int m_iYNumSeconds = -1;
+    std::chrono::steady_clock::time_point m_requestOperationTime;
+    int m_iQueryId = -1;
 };
 // to keep track of the order status    
 enum class OrderStatus {
@@ -122,17 +141,24 @@ public:
 //private: 
     int m_iId = -1;
     char m_cSide = 'U'; // unknown
-    double m_fPrice = -1;
-    int m_iQuantity = -1; 
+    double m_fPrice = 0;
+    int m_iQuantity = 0; 
     OrderStatus m_orderStatus = OrderStatus::sInit;
-    std::chrono::system_clock::time_point m_requestTime;
+    std::chrono::steady_clock::time_point m_requestTime;
+
+    int m_iQuantityFilled = 0; // use to calc the number of quantities are not filled yet. 
+    int m_iOldId = -1, m_iNewId = -1; // used to link the original insert request and the replace request.
 };
 
 class DerivedListener : public Listener
 {
 public:
-    DerivedListener(int iX, int iY);
+    DerivedListener(int iXNumRequest, int iYNumSeconds);
     ~DerivedListener();
+
+    // Note, call this before attach this listener to receive request. 
+    void startThreadProcessMessage(); 
+    void processMessage();
 
     // Interfaces fo Listener 
     void OnInsertOrderRequest(int id, char side, double price, int quantity) override;
@@ -141,22 +167,37 @@ public:
     void OnRequestRejected( int id)override;
     void OnOrderFilled( int id, int quantityFilled) override;
     
-    void processMessage();
+    // Interfaces for PartA
+    // has more than x requrest been received in the last y seconds? 
+    bool hasEnoughXRequestsForLastYSeconds() ; // (x,y) given in ctor.
+    bool hasEnoughXRequestsForLastYSeconds(int iXNumRequest, double iYNumSeconds) ;
+
 private:
     int m_iXNumRequest = -1; // from ctor,
     int m_iYNumSeconds = -1; // from ctor, 
+    
     std::thread m_thread; // thread to processMessage
+    std::atomic_bool m_bStopThread{false};
     std::queue<std::shared_ptr<BasicMsg> > m_msgQueue;
+    
     std::deque<OrderInfo> m_aOrderInfo;
+    std::unordered_map<int, int> m_mapOrderId2Idx; //map the order id to index of m_aOrderInfo.
+
+    std::atomic_bool m_bHasEnoughXRequestForLastYSecond{ false};
+    std::atomic<int> m_bFinishOperationHasEnoughXRequestForLastYSecond { -1};
+    std::mutex m_mtxXRequestYSecond;
+    std::condition_variable m_cvXRequestYSecond;
 };
 
-DerivedListener::DerivedListener(int iX, int iY) 
-    : m_iXNumRequest(iX), m_iYNumSeconds(iY)
-{
-    m_thread = std::thread(&DerivedListener::processMessage, this);
+DerivedListener::DerivedListener(int iXNumRequest, int iYNumSeconds) 
+    : m_iXNumRequest(iXNumRequest), m_iYNumSeconds(iYNumSeconds)
+{ 
 }
 DerivedListener::~DerivedListener()
-{
+{ 
+    std::cout << "Dtor.\n";
+    if (m_bStopThread == false)
+        m_bStopThread = true;
     m_thread.join();
 }
 void DerivedListener::OnInsertOrderRequest(int id, char side, double price, int quantity)
@@ -166,7 +207,9 @@ void DerivedListener::OnInsertOrderRequest(int id, char side, double price, int 
     pMsg->m_cSide = side;
     pMsg->m_fPrice = price;
     pMsg->m_iQuantity = quantity;
+    pMsg->m_requestTime = std::chrono::steady_clock::now();
     m_msgQueue.push(pMsg);
+    std::cout << "IOR, qsize " << m_msgQueue.size() << std::endl;
 }
 void DerivedListener::OnReplaceOrderRequest(int oldId, int newId, int deltaQuantity)
 {
@@ -174,7 +217,9 @@ void DerivedListener::OnReplaceOrderRequest(int oldId, int newId, int deltaQuant
     pMsg->m_iOldId = oldId;
     pMsg->m_iNewId = newId;
     pMsg->m_iDeltaQuantity = deltaQuantity;
+    pMsg->m_requestTime = std::chrono::steady_clock::now();
     m_msgQueue.push(pMsg);
+    std::cout << "ROR, qsize " << m_msgQueue.size() << std::endl;
 }
 void DerivedListener::OnRequestAcknowledged(int id)
 {
@@ -195,15 +240,146 @@ void DerivedListener::OnOrderFilled(int id, int quantityFilled)
     pMsg->m_iQuantityFilled = quantityFilled;
     m_msgQueue.push(pMsg);
 }
+void DerivedListener::startThreadProcessMessage()
+{
+    // when this function is invoke, "this" object should been constructed. 
+    m_thread = std::thread(&DerivedListener::processMessage, this);
+}
 void DerivedListener::processMessage()
 {
     // this function runs in a thread to handle messages.
-    while (!m_msgQueue.empty())
+    std::cout << "processMsg start.\n";
+ 
+    while (!m_bStopThread)
     {
-        std::shared_ptr<BasicMsg> pMsg = m_msgQueue.front();
-        if (pMsg->msgType() == MsgType::tInsertOrderRequest)
-        {
-            std::shared_ptr<MsgInsertOrderRequest> pM = std::dynamic_pointer_cast<MsgInsertOrderRequest>(pMsg);
+        if (m_msgQueue.empty()) {
+            //std::cout << ".q0.";
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
         }
+        else 
+        {
+            std::shared_ptr<BasicMsg> pMsg = m_msgQueue.front();
+            m_msgQueue.pop();
+            
+            if (pMsg->msgType() == MsgType::tInsertOrderRequest)
+            {
+                std::shared_ptr<MsgInsertOrderRequest> pM = std::dynamic_pointer_cast<MsgInsertOrderRequest>(pMsg);
+                OrderInfo oi(pM->m_id, pM->m_cSide, pM->m_fPrice, pM->m_iQuantity);
+                oi.m_requestTime = pM->m_requestTime;
+                oi.m_orderStatus = OrderStatus::sSubmited;
+
+                m_mapOrderId2Idx[oi.m_iId] = m_aOrderInfo.size();
+                m_aOrderInfo.push_back(oi);
+                std::cout << "Insert oi: " << oi.m_iId << ", at[" << m_aOrderInfo.size() -1 <<"]." << std::endl;
+            }   
+            else if (pMsg->msgType() == MsgType::tReplaceOrderRequest)
+            {
+                std::shared_ptr<MsgReplaceOrderRequest> pM = std::dynamic_pointer_cast<MsgReplaceOrderRequest>(pMsg);
+                assert(pM->m_iOldId != -1);
+                if (m_mapOrderId2Idx.find(pM->m_iOldId) != m_mapOrderId2Idx.end())
+                {
+                    assert(m_mapOrderId2Idx[pM->m_iOldId] < m_aOrderInfo.size());
+                    OrderInfo &oldOI = m_aOrderInfo[m_mapOrderId2Idx[pM->m_iOldId]];
+    
+                    OrderInfo newOI(pM->m_iNewId, oldOI.m_cSide, oldOI.m_fPrice, oldOI.m_iQuantity - oldOI.m_iQuantityFilled + pM->m_iDeltaQuantity);
+                    newOI.m_requestTime = pM->m_requestTime;
+                    newOI.m_orderStatus = OrderStatus::sSubmited;
+                    newOI.m_iOldId = pM->m_iOldId;
+                    oldOI.m_iNewId = pM->m_iNewId;
+
+                    m_mapOrderId2Idx[newOI.m_iId] = m_aOrderInfo.size();
+                    m_aOrderInfo.push_back(newOI); 
+                    std::cout << "Replace oi: " << newOI.m_iId << ", at[" << m_aOrderInfo.size() -1 <<"]." << std::endl;
+                } else {
+                    // todo: something goes wrong
+                }
+            }
+            else if (pMsg->msgType() == MsgType::tHasXRequestForLastYSeconds)
+            {
+                m_bFinishOperationHasEnoughXRequestForLastYSecond = -1;
+                m_bHasEnoughXRequestForLastYSecond = false;
+
+                std::shared_ptr<MsgHasXRequestForLastYSeconds> pM = std::dynamic_pointer_cast<MsgHasXRequestForLastYSeconds>(pMsg);
+
+                const int iOrderRequestCount = m_aOrderInfo.size();
+                std::cout << "hasXY(X"<< pM->m_iXNumRequest << ", Y" << pM->m_iYNumSeconds <<"), oi size " << iOrderRequestCount << std::endl;
+                if (iOrderRequestCount < pM->m_iXNumRequest)
+                    m_bHasEnoughXRequestForLastYSecond = false;
+
+                const OrderInfo &oi = m_aOrderInfo[iOrderRequestCount - pM->m_iXNumRequest];
+                std::chrono::duration<double> tPastTimeDurtation = pM->m_requestOperationTime - oi.m_requestTime;
+                double fPastTime = tPastTimeDurtation.count();
+                std::cout << "Past time: " << fPastTime << std::endl;
+                if (fPastTime < pM->m_iYNumSeconds)
+                    m_bHasEnoughXRequestForLastYSecond = true;
+
+                m_bFinishOperationHasEnoughXRequestForLastYSecond = pM->m_iQueryId;
+                m_cvXRequestYSecond.notify_all();
+            }
+        }
+    } // end of while 
+    std::cout << "processMsg end.\n";
+}
+
+bool DerivedListener::hasEnoughXRequestsForLastYSeconds()  
+{
+    return hasEnoughXRequestsForLastYSeconds(m_iXNumRequest, m_iYNumSeconds);
+}
+bool DerivedListener::hasEnoughXRequestsForLastYSeconds(int iXNumRequest, double iYNumSeconds)  
+{ 
+    static int iQueryId = 0;
+    
+    std::shared_ptr<MsgHasXRequestForLastYSeconds> pMsg = std::make_shared<MsgHasXRequestForLastYSeconds>();
+    pMsg->m_iXNumRequest = iXNumRequest;
+    pMsg->m_iYNumSeconds = iYNumSeconds;
+    pMsg->m_requestOperationTime = std::chrono::steady_clock::now();
+    pMsg->m_iQueryId = iQueryId++; // use the query id to support multi calls of this func at the similar time. 
+    m_msgQueue.push(pMsg);
+    std::cout << "Insert xy query, " << pMsg->m_iQueryId << ".\n";
+    if (iQueryId > INT32_MAX - 1)
+        iQueryId = 0; 
+
+    // wait until the calc finish
+	std::unique_lock<std::mutex> lk(m_mtxXRequestYSecond);
+	//m_cvXRequestYSecond.wait(lk, [&] { return m_bFinishOperationHasEnoughXRequestForLastYSecond == pMsg->m_iQueryId; }); 
+    while (m_bFinishOperationHasEnoughXRequestForLastYSecond != pMsg->m_iQueryId)
+    {
+        m_cvXRequestYSecond.wait(lk);
     }
+    m_bFinishOperationHasEnoughXRequestForLastYSecond = -1; // Note: this
+		    
+    return m_bHasEnoughXRequestForLastYSecond;
+}
+
+int main()
+{
+        DerivedListener *pListener = (new DerivedListener(10, 2));
+        pListener->startThreadProcessMessage();
+        //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    {
+    
+        for (int i = 0; i < 11; ++i)
+            pListener->OnInsertOrderRequest(i, 'B', 1.0, 1);
+
+        auto tStartTest1 = std::chrono::steady_clock::now();
+        bool bTest1Result = pListener->hasEnoughXRequestsForLastYSeconds(); // Answer of Part A (1). 
+        auto tEndTest1 = std::chrono::steady_clock::now();
+        if (bTest1Result == false)
+            std::cout << "Test fail 1.\n";
+        else
+            std::cout << "Test ok 1.\n";
+        // Answer of Part A (2).  Test 1 took time: 0.000249115s. Test 1 took time: 0.000371103s.
+        std::cout << "Test 1 took time: " << std::chrono::duration<double>(tEndTest1 - tStartTest1).count() << "s.\n";
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+        if (pListener->hasEnoughXRequestsForLastYSeconds() == false)
+            std::cout << "Test ok 2.\n";
+        else    
+            std::cout << "test fail 2.\n";
+        if (pListener->hasEnoughXRequestsForLastYSeconds() == false)
+            std::cout << "Test ok 3.\n";
+        else    
+            std::cout << "test fail 3.\n";
+    }
+    return 0;
 }
