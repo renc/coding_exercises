d85aa24fc063a070c548a55d802816b4fa2049e3
renc <rencanjiang@gmail.com>
2023-09-07 22:14:27 +0800

    add example

--- cpp/ex9_sfinae.cpp
+++ cpp/ex9_sfinae.cpp
@@ -2,55 +2,49 @@
 #include <vector>
 #include <string>
 #include <type_traits>
-    
-namespace c11 {
-    template <typename ...>
-    using void_t = void;
+
+// SFINAE from C++11
+// std::enable_if  , std::is_same_v
+// https://eli.thegreenplace.net/2014/sfinae-and-enable_if/
+template <typename T, typename std::enable_if_t<std::is_integral_v<T>, T>* = nullptr>
+void do_stuff(const T& t)
+{
+    std::cout << "do_stuff integral type\n";
 }
 
-// primary template
-template<typename, typename = void>
-struct has_type_member: std::false_type {};
-// partial specialization
-template<typename T>
-struct has_type_member<T, std::void_t<typename T::type>> : std::true_type {};
+template <typename T, typename std::enable_if_t<std::is_class_v<T>, T>* = nullptr>
+void do_stuff(const T& t)
+{
+    std::cout << "do_stuff class type\n";
+}
 
-// primary template 
-template <typename T,  typename = void>
-struct has_foo : std::false_type {};
-// partial specialization 
 template <typename T>
-struct has_foo<T, std::void_t<decltype(
-    std::declval<T&>().foo()
-)>> : std::true_type {};
+void do_stuff17(const T& t)
+{
+    if constexpr (std::is_integral_v<T>)
+    {
+        std::cout << "do_stuff17 integral type\n";
+    }
+    else if constexpr (std::is_class_v<T>)
+    {
+        std::cout << "do_stuff17 class type\n";
+    }
+}
 
-// primary template 
-template <typename T, typename = void>
-struct can_reference : std::false_type {};
-// partial specialization
-template <typename T>
-struct can_reference<T, std::void_t<T&>> : std::true_type{};
+// C++20 using concept
+//template <template T>
+//void do_stuff20
 
-// primary class template
-template<typename T>
-struct is_void {
-    static const bool value = false;
-};
-// "<>" means a full specialzation
-template<>
-struct is_void<void> {
-    static const bool value = true;
-};
+int main(int argc, char* argv[])
+{
+    int a = 10;
+    std::string ss("abc");
+    do_stuff<decltype(10)>(10); // (1)
+    do_stuff<std::string>(std::string("abc")); // (2)
+    // when (1), first overload works because std::enable_if<true, int>::type 
+    // while second overload  std::enable_if<false, int> the Primary template does not have ::type, so fail
 
-// primary class template 
-template<typename T>
-struct is_pointer {
-    static const bool value = false;
-};
-// "typename T" in "<>" means partial specialization
-template <typename T>
-struct is_pointer<T*> {
-    static const bool value = true;
-};
-
-
+    do_stuff17(ss);
+    do_stuff17(a);
+    return 0;
+}
