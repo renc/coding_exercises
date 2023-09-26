bc283f89dce8b213329b3593b438140a1e94d9e4
renc <rencanjiang@gmail.com>
2023-09-26 22:35:20 +0800

    update cpp

---
+++ cpp/ex2_strc.cpp
@@ -0,0 +1,22 @@
+#include <array>
+#include <type_traits>
+#include <iostream>
+
+// compile time string 
+
+constexpr std::uint32_t hash(const char* str)
+{
+    std::uint32_t v = 15;
+    while (char c = *str++)
+    {
+        v = (v << 2) + c; 
+    }
+    return v;
+    // char is 1 byte (8 bits)
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << hash("hello") << "\n";
+    std::cout << hash("world") << "\n";
+}
