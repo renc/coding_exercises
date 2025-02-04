bdb9946f2ac8ec3249b10a94df46ffeb6c12b9f9
renc <canjiang.ren@outlook.com>
2025-02-04 22:15:58 +0800

    ex2_str2int.cpp update

--- cpp/ex2_str2int.cpp
+++ cpp/ex2_str2int.cpp
@@ -21,10 +21,10 @@ uint32_t str2int(std::string_view s)
 }
 std::string int2str(uint32_t v)
 {
-    char c0 = char((v >> 24) & 0xFF);
-    char c1 = char(v >> 16 & 0xFF);
-    char c2 = (char)((v & 0x0000FF00) >> 8);
-    char c3 = (char)(v & 0xFF);
+    char c0 = char((v >> 24) & 0xFF);   // i was wrong c0 = v & 0xFF000000;
+    char c1 = char(v >> 16 & 0xFF);     // i was wrong c1 = v & 0x00FF0000;
+    char c2 = (char)((v & 0x0000FF00) >> 8); // i was wrong c2 = v & 0x0000FF00;
+    char c3 = (char)(v & 0xFF);         // i was wrong c3 = v & 0x000000FF;
     std::cout << "c0=" << c0 << ",c1=" << c1 << ",c2=" << c2 << ",c3=" << c3 << ".\n";
     std::string s = "0000";
     s[0] = c0; s[1] = c1; s[2] = c2; s[3] = c3;
